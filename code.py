bc2b46dbaf12df3389fe64e8645b6d09306dc929
Icaro Carvalhais <icarocarvalhais@gmail.com>
2021-07-06 18:37:21 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,13 @@
+water = int(input('Write how many ml of water the coffee machine has:\n'))
+milk = int(input('Write how many ml of milk the coffee machine has:\n'))
+beans = int(input('Write how many g of coffee beans the coffee machine has:\n'))
 cups = int(input('Write how many cups of coffee you will need:\n'))
-water = 200
-milk = 50
-beans = 15
-print(f'''For {cups} of coffee you will need:
-{cups * water} ml of water
-{cups * milk} ml of milk
-{cups * beans} g of coffee beans''')
+available = min(water // 200, milk // 50, beans // 15)
+
+if cups == available:
+    print('Yes, I can make that amount of coffee')
+elif cups > available:
+    print(f'No, I can make only {available} cups of coffee')
+else:
+    print(f'Yes, I can make that amount of coffee (and even {available - cups} more than that)')
+    
