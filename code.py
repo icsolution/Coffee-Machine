b6e1974b1c2399572c1187cfdf0d7d62d312c514
Icaro Carvalhais <icarocarvalhais@gmail.com>
2021-07-06 18:14:00 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+cups = int(input('Write how many cups of coffee you will need:\n'))
+water = 200
+milk = 50
+beans = 15
+print(f'''For {cups} of coffee you will need:
+{cups * water} ml of water
+{cups * milk} ml of milk
+{cups * beans} g of coffee beans''')
